2f2891da6ce3e41b6829005df162fdb69f716cc7
MisbahDayo <dayomuhammad892@gmail.com>
2024-03-29 12:41:37 +0500

    first commit

---
+++ space.js
@@ -0,0 +1,6 @@
+"use strict";
+// store a person name 
+Object.defineProperty(exports, "__esModule", { value: true });
+let personName = "Misbah Jameel";
+// printing  a  message
+console.log(`Hello, ${personName}  would you like to learn some typescript today?`);
---
+++ space.ts
@@ -0,0 +1,6 @@
+// store a person name 
+
+let personName:string= "Misbah Jameel"; 
+
+// printing  a  message
+console.log( `Hello, ${ personName}  would you like to learn some typescript today?`)
